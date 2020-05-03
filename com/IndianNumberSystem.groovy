d75f54d065720854e3e9285f63d8ba4c8121e052
Ashish <ashish@biztecno.net>
2020-05-03 20:49:21 +0530

    #1 -ve number

--- com/IndianNumberSystem.groovy
+++ com/IndianNumberSystem.groovy
@@ -1,5 +1,19 @@
 package com
+/**
 
+100000 pass
+1,00,000 pass
+10,00,000 pass
+10,000 pass
+1,000 pass
+10 pass
+1,00,00,000 pass
+
+10,00,00 fail
+100,000 fail
+1,000,000 fail
+10,000,000 fail
+*/
 
 def inputList =[
 	"100000",
@@ -25,11 +39,14 @@ def inputList =[
 	// fail
 	"10,000,000",
 	// fail
+
+	"-1,000"
+	// pass
 ]
 for (input in inputList) {
-	def matchers= (input =~ /^(?:\d+|\d{1,2},(?:\d{2},)*\d{3})$/)
+	def matchers= (input =~ /^-?(?:\d+|\d{1,2},(?:\d{2},)*\d{3})$/)
 	if(matchers.size()) {
 
 	}
-	println "${input} => ${matchers.size()?'valid':'invalid'}"
+	println "${input} => ${matchers.size()?'pass':'fail'}"
 }
