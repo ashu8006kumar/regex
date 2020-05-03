4c4de8357ab643860c44683dd158d53068586141
Ashish <ashish@biztecno.net>
2020-05-03 20:46:08 +0530

    #1 indian number regx

---
+++ com/IndianNumberSystem.groovy
@@ -0,0 +1,35 @@
+package com
+
+
+def inputList =[
+	"100000",
+	//pass
+	"1,00,000",
+	//pass
+	"10,00,000",
+	// pass
+	"10,000",
+	// pass
+	"1,000",
+	// pass
+	"10",
+	// pass
+	"1,00,00,000",
+	// pass
+
+	"10,00,00",
+	// fail
+	"100,000",
+	// fail
+	"1,000,000",
+	// fail
+	"10,000,000",
+	// fail
+]
+for (input in inputList) {
+	def matchers= (input =~ /^(?:\d+|\d{1,2},(?:\d{2},)*\d{3})$/)
+	if(matchers.size()) {
+
+	}
+	println "${input} => ${matchers.size()?'valid':'invalid'}"
+}
