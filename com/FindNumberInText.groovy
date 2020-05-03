92d21bd2393da7c0814d173038e6eb6c97d59f72
Ashish <ashish@biztecno.net>
2020-05-03 20:13:57 +0530

    comment update

--- com/FindNumberInText.groovy
+++ com/FindNumberInText.groovy
@@ -1,7 +1,7 @@
 package com
 /**
  * Find number Given TEXT
- * Pick only those which start with digit and with dist and no char in those
+ * Pick only those which start with digit ,end with digit and no char in those
  */
 String input ="This is an example from which you have to extract number like 1234  or 354 but not a123"
 def matchers= (input =~ /\b[\d]+\b/)
