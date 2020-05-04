bb5f205e723ed8e073914cc5f9aafa675af8bf28
Ashish <ashish@biztecno.net>
2020-05-04 07:03:56 +0530

    Case insensitive issue fixed #4

--- com/Email.groovy
+++ com/Email.groovy
@@ -9,8 +9,18 @@ package com
  * 6 . combination of chars and digits end with dot(.)
  * 7 . Ends with 2,10 char
  * 8 . optional dot(.) follow by char 2,10 
+ * 9 . case insensitive
  */
 
-String input ="some_thing.007@some.com"
-def matcher= (input =~ /^([a-z][a-z\d_\.]*)@([a-z][a-z\d]+\.)[a-z\d]{2,10}(?:\.[a-z]{2,10})?$/)
-matcher.size()?"Valid" :"Invalid"
+def  inputList = [
+	"Some_thing.007@some.com",
+	// valid
+	"some_thing.007@some.com",
+	//valid
+	"1some_thing.007@some.com",//Invalid
+]
+for (input in inputList) {
+	def matcher= (input =~ /^(?i)([a-z]{1}[a-z\d_\.]*)@([a-z][a-z\d]+\.)[a-z\d]{2,10}(?:\.[a-z]{2,10})?$/)
+	println matcher.size()?"Valid" :"Invalid"
+}
+
